cf99f83d2ade2a8243bb8d7aff6271b415d9b171
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-03 14:58:06 -0500

    Add initial code lines in 'details.js' in redux

---
+++ src/redux/details/details.js
@@ -0,0 +1,34 @@
+import { GET_DETAILS, GET_STOCK, GET_DETAILS_ERROR } from './detailsServices';
+
+const initialState = {
+  details: [
+    {
+      symbol: '',
+      name: '',
+      exchange: '',
+      price: 0,
+      previousClose: 0,
+      changesPercentage: '',
+      dayHigh: 0,
+      dayLow: 0,
+      yearHigh: 0,
+      yearLow: 0,
+    },
+  ],
+};
+
+
+const detailsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_STOCK:
+      return { ...state, pending: false, details: action.payload };
+    case GET_DETAILS:
+      return { ...state, pending: true };
+    case GET_DETAILS_ERROR:
+      return { ...state, pending: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default detailsReducer;
