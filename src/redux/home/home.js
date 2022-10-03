3b48c64fa71e0c3e16b45a3cbd7d6eae17598773
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-03 16:00:58 -0500

    Add code lines in file 'home.js' in redux

---
+++ src/redux/home/home.js
@@ -0,0 +1,59 @@
+import { GET_STOCKS, GET_STOCKS_SUCCESS, GET_STOCKS_ERROR } from '../slices/stocksServices';
+
+const initialState = { stocks: [], pending: false, error: null };
+
+const FILTER_STOCKS = 'FILTER_STOCKS';
+const RESTORE_STOCKS = 'RESTORE_STOCKS';
+
+const restoreStocks = (payload) => ({ type: RESTORE_STOCKS, payload });
+
+const filterStocks = (payload) => ({ type: FILTER_STOCKS, payload });
+
+const stocksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_STOCKS:
+      return {
+        ...state,
+        pending: true,
+      };
+    case GET_STOCKS_SUCCESS:
+      {
+        const { data } = action.stocks;
+        const subset = [];
+        data.$.map((stock) => {
+          subset.push({
+            symbol: stock.symbol,
+            price: stock.price,
+            name: stock.name,
+            exchange: stock.exchangeShortName,
+          });
+        });
+        return { 
+          ...state,
+          pending: false,
+          stocks: subset,
+        };
+      }
+    case GET_STOCKS_ERROR:
+      return {
+        ...state,
+        pending: false,
+        error: action.payload,
+      };
+    case FILTER_STOCKS:
+      return {
+        ...state,
+        stocks: state.stocks.filter((stock) => stock.symbol.toLowerCase().includes(action.payload)),
+      };
+    case RESTORE_STOCKS:
+      return {
+        ...state,
+        pending: false,
+        stocks: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export { stocksReducer, filterStocks, restoreStocks };
