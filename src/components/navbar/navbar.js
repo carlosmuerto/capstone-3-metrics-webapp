4416d6a13926198a9a84499344d7aa6f3c14df1a
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-03 21:21:32 -0500

    Modify 'navbar.js' file in components

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,23 +2,26 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { restoreStocks } from '../../redux/home/home';
+import { MdKeyboardArrowLeft, MdSearch } from 'react-icons/md';
+import { GoGear } from 'react-icons/go';
 
 const Navbar = () => {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const returnClick = () => {
-    dispatch(restoreStocks(state.stocks.stocks));
+    dispatch(restoreStocks(state));
   };
 
   return (
     <nav className="navbar">
-      <NavLink to="/" className="navbar__link" onClick={returnClick}>
-        <MdKeyboardArrowLeft className="navbar__icon" />
+      <NavLink to="/" className="navbar-link" onClick={returnClick}>
+        <MdKeyboardArrowLeft className="navbar-icon" />
       </NavLink>
-      <h2 className="navbar__title">Stock Market WebApp</h2>
-      <div className="navbar__search">
-        <MdSearch className="navbar__icon" />
+      <h2 className="navbar-title">Stock Market WebApp</h2>
+      <div className="navbar-search">
+        <MdSearch className="navbar-icon" />
+        <GoGear className="navbar-icon" />
       </div>
     </nav>
   );
